95fe479e0ccd840ceafaa72ce09cca66d4c5d373
pinky-banik <pinky.rani.banik@gmail.com>
2022-08-27 04:09:51 +0600

    initialize project and install packages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-primary text-8xl'>hello</h1>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
