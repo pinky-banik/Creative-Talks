9c1472f070c401447a22e49ade45344769ac6d4f
pinky-banik <pinky.rani.banik@gmail.com>
2022-09-02 01:44:23 +0600

    loader added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,20 +6,27 @@ import Sidebar from '../Components/Sidebar';
 import axios from 'axios';
 import { useLocation, useParams } from 'react-router-dom';
 import { Context } from './../context/Context';
+import loading from '../assets/loading.gif';
 
 const Home = () => {
     const [posts,setPosts] = useState([]);
+    const [loading,setLoading] = useState(false);
     const {search} = useLocation();
-    const {user} = useContext(Context);
-    // console.log(user || "no user");
+
     useEffect(()=>{
         const fetchPosts = async() => {
+            setLoading(true);
           const res = await axios.get("https://glacial-everglades-76553.herokuapp.com/api/posts"+search);
         //   console.log(res);
           setPosts(res.data);
+          setLoading(false);
         }
         fetchPosts();
-    },[search])
+    },[search]);
+    if(loading)
+    {
+        return <div className='flex justify-center items-center max-h-screen'><img src={loading} alt="" /></div>
+    }
     return (
         <div>
             <Header/>
